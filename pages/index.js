aaff1575b9317629755f86c0db5969cdae90f80e
syahman9212-eng <syah_man9212@yahoo.com>
2026-08-09 01:37:40 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     try {
       window.Pi.init({
         version: "2.0",
-        sandbox: true,
+        sandbox: false,
       });
 
       await window.Pi.authenticate(
