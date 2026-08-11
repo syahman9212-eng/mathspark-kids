80994607b49d47b365710bcd0d3aa73746996241
syahman9212-eng <syah_man9212@yahoo.com>
2026-08-11 17:42:48 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -160,6 +160,7 @@ if (premiumResponse.ok && premiumResult.premium) {
 
           if (response.ok) {
             setIsPremium(true);
+            window.location.href = "/index.html";
           } else {
             alert("Payment completion failed");
           }
